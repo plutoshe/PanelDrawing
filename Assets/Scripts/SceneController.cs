6857b6ca0435a1c2a30a6dff5fcc27af57135a4a
plutoshe <plutoshe@gmail.com>
2019-02-23 18:41:35 -0700

    add preface

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+    static SceneController _instance;
+    public static SceneController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new SceneController();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        if (_instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LoadScene(string sceneName)
+    {
+        Debug.Log("Go to Scene:" + sceneName + "\n");
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+}
