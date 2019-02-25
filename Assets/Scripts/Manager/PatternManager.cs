093460ef11422eea9d2b1ecec220dc27dff510fb
plutoshe <plutoshe@gmail.com>
2019-02-24 21:16:07 -0700

    drawing basic panel setting

---
+++ Assets/Scripts/Manager/PatternManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PatternManager : MonoBehaviour {
+
+    static PatternManager _instance;
+    public List<PatternAttr> Patterns;
+    public Transform PatternDisplayPanel;
+    public GameObject PatternItemPrefab;
+    public static PatternManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new PatternManager();
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        if (_instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < Patterns.Count; i++)
+        {
+            var newPattern = Instantiate(PatternItemPrefab);
+            newPattern.GetComponent<PatternItem>().attr = Patterns[i];
+            newPattern.transform.parent = PatternDisplayPanel;
+            newPattern.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PatternItem.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+[Serializable]
+public class PatternAttr
+{
+    public Sprite DisplayImage;
+}
+[RequireComponent(typeof(Image))]
+[RequireComponent(typeof(Button))]
+public class PatternItem : MonoBehaviour
+{
+    public PatternAttr attr;
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Image>().sprite = attr.DisplayImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
