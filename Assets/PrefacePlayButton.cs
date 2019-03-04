9a4c38903c2605e9defb5786f144fd51fab25e3d
Emma <zzzemmaty@gmail.com>
2019-03-03 20:38:00 -0700

    udpate UI for previous scenes

---
+++ Assets/PrefacePlayButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrefacePlayButton : MonoBehaviour
+{
+    public string gotoScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        SceneController.Instance.LoadScene(gotoScene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
